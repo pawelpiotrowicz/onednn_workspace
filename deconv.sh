884becab2834b955a0743390c8820cf8523b657d
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2023-03-03 17:07:46 +0000

    save

--- deconv.sh
+++ deconv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 bn="/libraries.performance.math.onednn/build/tests/benchdnn/benchdnn"
 export DNNL_VERBOSE=3
+#export CreateMultipleSubDevices=1
+#export ZE_AFFINITY_MASK=0.2
 usage() {
 
-echo "exit() : --all --postop64 --simple32 --simple64 --small --fail --conv-all"
+echo "exit() : --all --all_ci --postop64 --simple32 --simple64 --small --fail --conv-all"
 exit
 }
 
@@ -16,6 +18,17 @@ if [ "$1" == "--conv-all" ]; then
 ./$bn --conv --engine=gpu  --batch=test_conv_gpu 
 exit;
 fi
+
+
+
+if [ "$1" == "--all_ci" ]; then
+./$bn --deconv --engine=gpu --batch=test_deconv_ci
+#./$bn --deconv --engine=gpu --mode=l --batch=test_deconv_ci
+exit;
+fi
+
+
+
 
 if [ "$1" == "--all" ]; then
 ./$bn --deconv --engine=gpu --batch=test_deconv_gpu
@@ -54,10 +67,17 @@ fi
 
 
 if [ "$1" == "--fail" ]; then
-./$bn --deconv --engine=gpu --dir=BWD_WB --cfg=bf16f32bf16 mb16ic128ih3oc48oh7kh5ph2dh1n"5b/5x5"
-./$bn --deconv --engine=gpu --dir=BWD_WB --cfg=f64f64f64 mb16ic128ih3oc48oh7kh5ph2dh1n"5b/5x5"
+#./$bn --deconv --engine=gpu --dir=BWD_WB --cfg=bf16f32bf16 mb16ic128ih3oc48oh7kh5ph2dh1n"5b/5x5"
+#./$bn --deconv --engine=gpu --dir=BWD_WB --cfg=f64f64f64 mb16ic128ih3oc48oh7kh5ph2dh1n"5b/5x5"
 #./$bn --deconv --engine=gpu --dir=BWD_WB --cfg=f64f32bf16 mb16ic128ih3oc48oh7kh5ph2dh1n"5b/5x5"
 
+#./$bn --deconv --engine=gpu  --cfg=f64 g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
+#./$bn --conv --engine=gpu --cfg=f64 --dir=BWD_D g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
+#./$bn --conv --engine=gpu --cfg=f64 --dir=FWD_B g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
+#./$bn --conv --engine=gpu --mb=2 --cfg=f64 --dir=FWD_D g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
+
+./$bn --deconv --engine=gpu  --cfg=f64  g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
+./$bn --deconv --engine=gpu  --cfg=f64 --stag=axb g4ic16iw5oc16ow5kw3pw1n"1d_conv:grouped"
 exit;
 fi
 
